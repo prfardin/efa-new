b730d79196e7f8abd8100d3d481792d24953c12f
Fardin Moradi <prfardin@gmail.com>
2022-12-20 19:36:00 +0330

    All Monochrome Icons and some Less changes for ui

--- resources/back-office/components/sidebar/strip.vue
+++ resources/back-office/components/sidebar/strip.vue
@@ -3,7 +3,7 @@
 </script>
 
 <template>
-    <div class="pr-sidebar-strip">
+    <div class="pr-sidebar-strip" style="z-index: 1">
         <div class="pr-logo-area uk-flex uk-flex-top uk-flex-center">
             <span uk-icon="user" style="color: white;border-radius: 100%;background-color: #03a9f4;width: 44px;height: 44px;display: flex;align-items: center;justify-content: center;"></span>
         </div>
@@ -11,10 +11,22 @@
             <nav>
                 <ul class="uk-nav">
                     <li class="uk-active">
-                        <a href="#" uk-tooltip="title: داشبورد; pos: left; offset: 3;duration: 200" uk-icon="dashboard2"></a>
+                        <a href="#" uk-tooltip="title: داشبورد; pos: left; offset: 3;duration: 200" uk-icon="dashboard"></a>
+                    </li>
+                    <li class="uk-active">
+                        <a href="#" uk-tooltip="title: داشبورد; pos: left; offset: 3;duration: 200" uk-icon="comments-alt"></a>
+                    </li>
+                    <li class="uk-active">
+                        <a href="#" uk-tooltip="title: داشبورد; pos: left; offset: 3;duration: 200" uk-icon="dashboard"></a>
+                    </li>
+                    <li class="uk-active">
+                        <a href="#" uk-tooltip="title: داشبورد; pos: left; offset: 3;duration: 200" uk-icon="dashboard"></a>
+                    </li>
+                    <li class="uk-active">
+                        <a href="#" uk-tooltip="title: داشبورد; pos: left; offset: 3;duration: 200" uk-icon="dashboard"></a>
                     </li>
                 </ul>
-                <div class="line" style="top: 0; height: 52px;"></div></nav>
+                <div class="line" style="top: 5px; transform: translate(-50%, 0px); left: 50%; width: 42px; height: 42px; z-index: -1; border-radius: 8px; background: rgb(255, 255, 255);"></div></nav>
         </div>
     </div>
 </template>
