1ed777409e86a77dddd66fbeb440176de9e3d8ce
Fardin Moradi <prfardin@gmail.com>
2023-04-10 17:29:42 +0430

    Latest Changes

--- resources/back-office/components/sidebar/strip.vue
+++ resources/back-office/components/sidebar/strip.vue
@@ -26,19 +26,13 @@ import Avatar from '@bi/avatar.png'
                     <li><pr-link to="/about" icon="star" :ratio="0.7" class="pr-icon-favourite" /></li>
                     <li><pr-link to="/about" icon="create-dashboard" :ratio="0.8" /></li>
                     <li><pr-link to="/about" icon="folder-minus" class="uk-light" :ratio="0.8" /></li>
-                    <li class="pr-item-has-notification">
-                        <pr-link to="/about" icon="compass" :ratio="0.8" />
-                    </li>
-                    <li class="pr-item-has-badge">
-                        <pr-link to="/about" icon="bell" :ratio="0.8" slot-class="pr-sidebar-strip-badge">2</pr-link>
-                    </li>
+                    <li class="pr-item-has-notification"><pr-link to="/about" icon="compass" :ratio="0.8" /></li>
+                    <li class="pr-item-has-badge"><pr-link to="/about" icon="bell" :ratio="0.8" slot-class="pr-sidebar-strip-badge">2</pr-link></li>
                     <li><pr-link to="/about" icon="notebooks" :ratio="0.8" /></li>
                     <li><pr-link to="/about" icon="bag" :ratio="0.8" /></li>
                     <li class="uk-nav-divider"></li>
-                    <li style="margin-top: 28px;">
-                        <pr-link to="/about">
-                            <pr-icon style="background: rgb(255, 255, 255); box-shadow: 0 4px 14px rgba(0, 0, 0, 0.1);width: 28px; height: 28px; border-radius: 8px" icon="file-alt" :ratio="0.65"></pr-icon>
-                        </pr-link>
+                    <li class="pr-sidebar-strip-accordion">
+                        <pr-link to="/about" icon="file-alt" :ratio="0.65"></pr-link>
                         <pr-link style="margin-top: -10px" to="/about" icon="angle-down" :ratio="0.8" />
                     </li>
                     <li style="margin-top: 18px;">
@@ -47,7 +41,7 @@ import Avatar from '@bi/avatar.png'
                 </pr-nav>
                 <div class="line" style="top: 0; transform: translate(50%, 0px); right: 50%; width: 36px; height: 36px; z-index: -1; border-radius: 10px; background: #f0f4f7;"></div>
                 <div class="line" style="top: 128px; transform: translate(50%, 0px); right: 50%; width: 36px; height: 36px; z-index: -1; border-radius: 10px; background: #2785ff;"></div>
-                <div class="line" style="top: 382px; transform: translate(50%, 0px); right: 50%; width: 40px; height: 60px; z-index: -1; border-radius: 10px; background: rgb(255, 255, 255); box-shadow: 0 13px 20px rgba(0, 0, 0, 0.1);"></div>
+<!--                <div class="line" style="top: 382px; transform: translate(50%, 0px); right: 50%; width: 40px; height: 60px; z-index: -1; border-radius: 10px; background: rgb(255, 255, 255); box-shadow: 0 13px 20px rgba(0, 0, 0, 0.1);"></div>-->
                 <div class="line" style="top: 462px; transform: translate(50%, 0px); right: 50%; width: 36px; height: 36px; z-index: -1; border-radius: 100%; background: #2785ff;"></div>
             </nav>
         </div>
