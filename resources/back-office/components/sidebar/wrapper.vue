b5e9503778c95330419449525490dfb473be6cfd
prfardin <prfardin@gmail.com>
2023-04-15 04:06:17 +0430

    Latest Changes

--- resources/back-office/components/sidebar/wrapper.vue
+++ resources/back-office/components/sidebar/wrapper.vue
@@ -5,12 +5,13 @@ import PrLink from '@bc/core/link.vue'
 import PrIcon from '@bc/core/icon.vue'
 import PrNav from '@bc/core/nav.vue'
 import Avatar from '@bi/avatar.png'
+import ColorCircle from '@bi/color_circle.svg'
 
 const { t } = useI18n();
 </script>
 
 <template>
-    <div class="pr-sidebar-wrapper uk-light pr-light" style="z-index: 1;">
+    <div class="pr-sidebar-wrapper uk-light pr-light">
         <pr-link class="pr-sidebar-accordion" to="#" icon="angle-right" :ratio="0.8"></pr-link>
         <div class="pr-sidebar-header">
             <div class="pr-sidebar-header-wrapper">
@@ -41,7 +42,7 @@ const { t } = useI18n();
                         </pr-link>
                     </li>
                     <li>
-                        <pr-link to="/about" icon="folder-minus" :ratio="0.8">{{ t('sidebar.manage_products') }}</pr-link>
+                        <pr-link class="uk-active" to="/about" icon="folder-minus" :ratio="0.8">{{ t('sidebar.manage_products') }}</pr-link>
                     </li>
                     <li>
                         <pr-link class="pr-item-has-notification" to="/about" icon="compass" :ratio="0.8">{{ t('sidebar.orders') }}</pr-link>
@@ -76,47 +77,30 @@ const { t } = useI18n();
                             </template>
                         </pr-link>
                     </li>
-
-                    <li class="uk-nav-divider" style="border-color: #252726"></li>
-                    <li class="uk-nav-header" style="font-size: 10px; padding: 10px 10px; color: #858687; margin-top: 14px; margin-bottom: 2px">در حال انجام <span style="color: #fff">12</span></li>
-                    <li style="margin-bottom: 7px; display: flex">
-                        <pr-link style="position: relative; color: #bfbfbf; padding-right: 8px; padding-left: 4px" to="/about">
-                            <pr-icon style="background: #3c4041; box-shadow: 0 4px 14px rgba(0, 0, 0, 0.1);width: 26px; height: 26px; border-radius: 8px; display: flex; align-items: center;justify-content: center" icon="file-alt" :ratio="0.65"></pr-icon>
+                    <li class="uk-nav-divider"></li>
+                    <li class="uk-nav-header">{{ t('sidebar.in_progress') }} <span class="uk-text-primary">12</span></li>
+                    <li class="pr-sidebar-icon-group pr-sidebar-item-muted">
+                        <pr-link class="uk-active" to="/about" icon="file-alt" :ratio="0.65"></pr-link>
+                        <pr-link to="/about" icon="download-alt" :ratio="0.65"></pr-link>
+                        <pr-link to="/about" icon="user" :ratio="0.65">
+                            <template #after>
+                                <span class="pr-sidebar-item-badge pr-sidebar-badge-primary">5</span>
+                            </template>
                         </pr-link>
-                        <pr-link style="position: relative; color: #bfbfbf;  padding-right: 8px; padding-left: 8px" to="/about">
-                            <pr-icon icon="arrows-merge" :ratio="0.9"></pr-icon>
-                        </pr-link>
-                        <pr-link style="position: relative; color: #bfbfbf;  padding-right: 8px; padding-left: 8px" to="/about">
-                            <pr-icon icon="user" :ratio="0.65"></pr-icon>
-                        </pr-link>
-                        <pr-link style="position: relative; color: #bfbfbf;  padding-right: 8px; padding-left: 8px" to="/about">
-                            <pr-icon icon="chart-pie-alt" :ratio="0.65"></pr-icon>
-                        </pr-link>
-                        <pr-link style="position: relative; color: #bfbfbf;  padding-right: 8px; padding-left: 8px" to="/about">
-                            <span style="position: absolute; top: 3px; right: -48px; box-sizing: border-box; width: 16px; height: 16px; background-color: #2884ff; border-radius: 100%; border: 2px solid #252628; font-size: 7px; color: rgb(255, 255, 255); text-align: center; padding: 2px 4px;">5</span>
-                            <pr-icon icon="setting" :ratio="0.65"></pr-icon>
-                        </pr-link>
-                        <pr-link style="position: absolute; color: #bfbfbf;  left: 4PX" to="/about">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 23.88 23.88">
-                                <g>
-                                    <path d="M1.6,6a11.94,11.94,0,0,0,0,11.94C1,13,3.92,9.22,6.88,9c2.74-.21,4.87,2.67,5.06,2.94.13-.3,1.56-3.58,0-5.85C10.26,3.64,5.54,2.94,1.6,6Z" fill="#ffcd03"/>
-                                    <path d="M22.28,6A11.93,11.93,0,0,0,11.94,0C16.55,1.92,18.31,6.38,17,9c-1.19,2.47-4.76,2.87-5.08,2.9.19.26,2.32,3.14,5.06,2.93C20,14.64,22.93,10.9,22.28,6Z" fill="#ff5144"/>
-                                    <path d="M11.94,0A11.92,11.92,0,0,0,1.6,6c3.94-3,8.66-2.33,10.34.12,1.56,2.27.13,5.55,0,5.85.32,0,3.89-.43,5.08-2.9C18.31,6.38,16.55,1.92,11.94,0Z" fill="#ffa403"/>
-                                    <path d="M22.28,6c.65,4.93-2.32,8.67-5.28,8.9-2.74.21-4.87-2.67-5.06-2.93-.14.3-1.53,3.56,0,5.82,1.68,2.44,6.38,3.15,10.33.15a11.92,11.92,0,0,0,1.6-6A11.94,11.94,0,0,0,22.28,6Z" fill="#bd61d4"/>
-                                    <path d="M11.94,23.88a12,12,0,0,0,10.34-6c-4,3-8.65,2.29-10.33-.15-1.54-2.26-.15-5.52,0-5.82-.32,0-3.87.45-5.06,2.94C5.6,17.56,7.36,22,11.94,23.88Z" fill="#1ba6f7"/>
-                                    <path d="M1.6,17.91a11.94,11.94,0,0,0,10.34,6c-4.58-1.89-6.34-6.32-5.06-9,1.19-2.49,4.74-2.91,5.06-2.94C11.75,11.67,9.62,8.79,6.88,9,3.92,9.22,1,13,1.6,17.91Z" fill="#52ca27"/>
-                                </g>
-                            </svg>
+                        <pr-link to="/about" icon="chart-pie-alt" :ratio="0.65"></pr-link>
+                        <pr-link to="/about" icon="setting" :ratio="0.65"></pr-link>
+                        <pr-link class="pr-sidebar-icon-flip" to="/about">
+                            <span class="uk-icon uk-preserve">
+                                <img :src="ColorCircle" alt="color">
+                            </span>
                         </pr-link>
                     </li>
                 </pr-nav>
-                <div class="line" style="position: absolute; top: 128px; right: 0; left: 0; height: 36px; z-index: -1; border-radius: 10px; background: #2785ff;"></div>
-                <div class="line" style="position: absolute; top: 406px; right: 0; left: 0; height: 36px; z-index: -1; border-radius: 12px; background: #252628;"></div>
-                <div class="test line uk-flex uk-flex-center uk-flex-middle" style='position: absolute; top: 462px; right: 0; left: 0; height: 122px; z-index: -1; border-radius: 12px; background-color: #1c1d1f; '>
-                    <div class="uk-text-center">
-                        <pr-button style="background-color: rgb(39, 133, 255)" class="uk-button-rounded uk-button-primary test2" to="/about" icon="plus" icon-button default :ratio="0.7" />
-                        <h6 class="uk-margin-small uk-margin-remove-bottom" style="color: #ffffff">اضافه کردن قابلیت جدید</h6>
-                        <h6 class="uk-margin-remove uk-text-small" style="color: #ffffff; font-size: 9px; margin-top: 6px !important">مشاهده در <span class="uk-link" style="color: #fbda6a">بخش آموزش ها</span></h6>
+                <div class="pr-sidebar-advertising">
+                    <div class="uk-margin-remove-last-child">
+                        <pr-button to="/about" icon="line-plus" icon-button rounded primary :ratio="0.7" />
+                        <h6 class="uk-margin-small uk-margin-remove-bottom">{{ t('sidebar.new_capability') }}</h6>
+                        <h6 class="pr-margin-xsmall pr-text-xxsmall">{{ t('sidebar.see_in') }} <span class="uk-link pr-link-golden">{{ t('sidebar.learning_school') }}</span></h6>
                     </div>
                 </div>
             </nav>
