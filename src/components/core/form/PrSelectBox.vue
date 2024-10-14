8d3a6fa0736d6d2ed71232720d78ac9cff3403e2
MrSemicolon01 <abbasi.work.ali@gmail.com>
2024-10-14 16:35:02 +0330

    latest

--- src/components/core/form/PrSelectBox.vue
+++ src/components/core/form/PrSelectBox.vue
@@ -4,13 +4,15 @@ import { SelectBoxPropsType } from '@u/props'
 import { drop, RefElement } from '@u/util'
 import { ref, onMounted } from 'vue'
 import UIkit from 'uikit'
+import PrIcon from '@c/core/PrIcon.vue'
 
 // set props with defaults
 const props = withDefaults(defineProps<SelectBoxPropsType>(), {
   mode: 'click',
-  node: 'name',
+  node: 'title',
   id: 'id',
-  type: 'text'
+  type: 'text',
+  searchable: false
 })
 
 let dGet = true
@@ -30,15 +32,13 @@ const isOpen = ref<boolean>(false)
 
 onMounted(() => {
   drop(el.value, {
-    duration: 100,
-    // @ts-ignore
+    duration: 300,
     mode: props.mode,
     pos: 'bottom-center',
     offset: 5,
     boundary: '.pr-select',
-    // @ts-ignore
     stretch: 'x',
-    flip: false
+    flip: false,
   })
 
   // we write this for handle show and hide drop
@@ -64,30 +64,55 @@ function clicked(list: any) {
   drop(el.value).hide(false)
 }
 
+const inputProps = {
+  type: props.type,
+  name: props.name,
+  id: props.name
+}
+
+const inputRef = ref<any>(null)
+const selectRef = ref<any>(null)
+
+onMounted(() => {
+  if (props.searchable) {
+    selectRef.value.addEventListener("click", (e: any) => {
+
+    })
+  }
+})
+
+
+
+if (props.searchable) {
+  const test = function() {
+    inputRef.value.focus()
+  }
+}
+
 </script>
 
 <template>
-  <div>
-    <div class="pr-select">
-      <div class="uk-position-relative" :class="{ 'open': isOpen }">
-        <input class="uk-select"
-         readonly
-         :type="type"
-         :name="name"
-         :id="name"
-         v-model="model"
+  <div class="uk-position-relative">
+    <div class="pr-select" style="height: 23px!important;" :class="{ 'pr-searchable': searchable }" @click="test">
+      <div class="pr-select-item" :class="{ 'open': isOpen }">
+        <input class="pr-select-input"
+               v-bind="inputProps"
+               v-model="model"
+               ref="inputRef"
         />
-        <span class="pr-select-angle" :class="{divider: props.divider}"></span>
+        <span class="pr-select-input-chevron" :class="{ divider: props.divider }">
+          <pr-icon icon="line-angle-down" />
+        </span>
       </div>
-      <div class="pr-option-box" ref="el">
-        <div class="scrollbar">
-          <ul class="uk-nav uk-dropdown-nav">
-            <li v-for="(list, index) in lists" :key="index">
-              <a class="pr-option" @click="clicked(list)" :class="{'uk-active': list === model}">{{ list[node] }}</a>
-            </li>
-            <li class="uk-text-center" v-if="!lists?.length">هیچ داده ای پیدا نشد</li>
-          </ul>
-        </div>
+    </div>
+    <div class="pr-option-box" ref="el">
+      <div class="scrollbar">
+        <ul class="uk-nav uk-dropdown-nav">
+          <li v-for="(list, index) in lists" :key="index">
+            <a class="pr-option" @click="clicked(list)" :class="{'uk-active': list === model}">{{ list[node] }}</a>
+          </li>
+          <li class="uk-text-center" v-if="!lists?.length">هیچ داده ای پیدا نشد</li>
+        </ul>
       </div>
     </div>
   </div>
@@ -96,88 +121,6 @@ function clicked(list: any) {
 
 <style scoped>
 
-.uk-select {
-  background: #fdfdfd;
-  padding-left: 8px;
-  display: block;
-}
 
-.uk-select:focus {
-  background: #fdfdfd;
-  border-color: #ededf2;
-}
-
-.pr-select-angle {
-  width: 40px;
-  height: 36px;
-  position: absolute;
-  right: 0;
-  bottom: 0;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  box-sizing: border-box;
-}
-
-.pr-select-angle::after {
-  content: '';
-  display: inline-block;
-  width: 24px;
-  height: 24px;
-  background-color: #d3d3df;
-  -webkit-mask-image: url(@i/core/unicons/line/icons/angle-down.svg);
-  mask-image: url(@i/core/unicons/line/icons/angle-down.svg);
-  transition:transform .2s ease-in-out;
-}
-
-
-
-.open .pr-select-angle::after {
-  transform: rotate(0.5turn);
-}
-
-.divider::before {
-  content: '';
-  position: absolute;
-  display: inline-block;
-  border-left: 1px solid #ededf2;
-  height: 18px;
-  left: 0;
-}
-
-.uk-select:not([multiple]):not([size]) {
-  height: 36px;
-  padding-right: 40px;
-
-}
-
-.pr-option-box {
-  border: 1px solid #ededf2;
-  padding: 8px;
-  background-color: #fdfdfd;
-  overflow: auto;
-  max-height: 240px;
-}
-
-.pr-option-box.uk-open {
-  border-color: #015eff;
-}
-
-.pr-option {
-  color: #464964;
-  align-items: center;
-  cursor: pointer;
-  display: flex;
-  gap: .5rem;
-  padding: .5rem .75rem;
-  position: relative;
-  transition: color, background-color, border-color, text-decoration-color, fill, stroke .2s ease-in-out;
-  -webkit-user-select: none;
-  user-select: none;
-}
-
-.pr-option:hover {
-  background: rgba(1, 94, 255, 0.1)
-}
 
 </style>
--- src/views/TestComponent.vue
+++ src/views/TestComponent.vue
@@ -1,51 +1,24 @@
 <script setup lang="ts">
-import { onMounted, ref, watchEffect } from 'vue'
+import { ref } from 'vue'
 import PrContainer from '@c/core/PrContainer.vue'
 import PrSection from '@c/core/PrSection.vue'
-import PrTable from '@c/core/tables/table/PrTable.vue'
-import PrTableHeading from '@c/core/tables/table/PrTableHeading.vue'
-import PrCheckBox from '@c/core/form/PrCheckBox.vue'
-import PrFlexTableRow from '@c/core/tables/flex-table/PrFlexTableRow.vue'
-import PrTableCell from '@c/core/tables/table/PrTableCell.vue'
-import PrTableRow from '@c/core/tables/table/PrTableRow.vue'
-import PrPagination from '@c/core/PrPagination.vue'
-import { useRoute } from 'vue-router'
-import { notify } from '@u/util'
-import PrButton from '@c/core/PrButton.vue'
-import PrGrid from '@c/core/PrGrid.vue'
-import PrButtonGroup from '@c/core/PrButtonGroup.vue'
+import PrSelectBox from '@c/core/form/PrSelectBox.vue'
 
 const url = ref(`/`)
 
 const data = ref<any>([
-  {title: 'one', content: 'ttt'},
-  {title: 'two', content: 'tttt'},
-  {title: 'three', content: 'ttttt'},
-  {title: 'four', content: 'tttttt'}
+  {title: 'ttt', content: 'ttt'},
+  {title: 'tttt', content: 'tttt'},
+  {title: 'ttttt', content: 'ttttt'},
+  {title: 'tttttt', content: 'tttttt'}
 ])
-
-
-function test() {
-  notify("<span uk-icon='icon: line-check'></span><span>salam</span>")
-}
-
 
 </script>
 
 <template>
   <pr-section>
     <pr-container small>
-      <div class="uk-margin-bottom">
-        <pr-button mode="pastel" tag="button" color="secondary" rounded="md">test</pr-button>
-
-      </div>
-      <pr-button-group>
-          <pr-button mode="pastel" tag="button" color="secondary" rounded="md">test</pr-button>
-          <pr-button mode="pastel" tag="button" color="secondary" icon="line-user" rounded="md">test</pr-button>
-          <pr-button mode="pastel" tag="button" color="success" rounded="md">test</pr-button>
-          <pr-button mode="pastel" tag="button" color="warning" icon="line-user" ratio=".7" rounded="md">test</pr-button>
-          <pr-button mode="pastel" tag="button" color="danger" rounded="md">test</pr-button>
-      </pr-button-group>
+      <pr-select-box name="test" v-model="data.content" :lists="data" searchable />
     </pr-container>
   </pr-section>
 </template>
