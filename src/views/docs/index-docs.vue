640109c7c89995fc9d424f5f1369696b17a6a215
prfardin <prfardin@gmail.com>
2025-02-04 07:07:30 +0330

    fa icons added

--- src/views/docs/index-docs.vue
+++ src/views/docs/index-docs.vue
@@ -2,18 +2,11 @@
 
 import PrIcon from '@c/core/PrIcon.vue'
 
-/**
- * must remove:
- *
- *  */
+
 </script>
 
 <template>
     <div class="uk-background-default">
-        <pr-icon icon="icon-ming-fill-corner-down-left" />
-        <pr-icon icon="icon-ming-sharp-arrow-left-down" />
-        <pr-icon icon="icon-ming-twotone-arrow-left-down" />
-        <pr-icon icon="icon-ming-twotone-abs" />
-      <pr-icon icon="icon-ming-twotone-ad-rectangle" />
+        <pr-icon icon="icon-fa-duotoneso-exclamation" />
     </div>
 </template>
