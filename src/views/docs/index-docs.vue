7dddc5302cdc5cc58a5f64fe855f85a687b113bb
prfardin <prfardin@gmail.com>
2025-02-07 01:05:01 +0330

    uikit fix

--- src/views/docs/index-docs.vue
+++ src/views/docs/index-docs.vue
@@ -7,8 +7,14 @@ import PrIcon from '@c/core/PrIcon.vue'
 
 <template>
     <div class="uk-background-default">
-        <pr-icon icon="icon-huge-bulk-3d-scale" />
-        <pr-icon icon="icon-huge-bulk-abacus" />
-        <pr-icon icon="icon-huge-bulk-church" />
+        <pr-icon icon="icon-huge-bulk-3d-scale" ratio="2" />
+        <pr-icon icon="icon-huge-bulk-abacus" ratio="2" />
+        <pr-icon icon="icon-huge-bulk-church" ratio="2" />
+        <pr-icon icon="icon-huge-bulk-teaching" ratio="2" />
+        <pr-icon icon="icon-huge-bulk-google-lens" ratio="2" />
+        <pr-icon icon="icon-huge-duotone-3d-scale" ratio="2" />
+        <pr-icon icon="icon-huge-duotone-abacus" ratio="2" />
+        <pr-icon icon="icon-huge-duotone-church" ratio="2" />
+        <pr-icon icon="icon-huge-duotone-google-lens" ratio="2" />
     </div>
 </template>
