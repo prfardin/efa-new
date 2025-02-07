a681689e4e480e40cb5ae94e352426ce47c8e33a
prfardin <prfardin@gmail.com>
2025-02-07 23:54:29 +0330

    huge icons fix

--- src/views/docs/index-docs.vue
+++ src/views/docs/index-docs.vue
@@ -7,8 +7,14 @@ import PrIcon from '@c/core/PrIcon.vue'
 
 <template>
     <div class="uk-background-default">
+        <pr-icon icon="icon-huge-bulk-google-lens" ratio="2" />
         <pr-icon icon="icon-huge-bulk-3d-move" ratio="2" />
         <pr-icon icon="icon-huge-duotone-3d-move" ratio="2" />
+        <pr-icon icon="icon-huge-duotone-acute" ratio="2" />
+        <pr-icon icon="icon-huge-duotone-layers-02" ratio="2" />
+        <pr-icon icon="icon-huge-duotone-note-edit" ratio="2" />
+        <pr-icon icon="icon-huge-duotone-task-edit-01" ratio="2" />
+        <pr-icon icon="icon-huge-duotone-clip" ratio="2" />
         <pr-icon icon="icon-huge-twotone-3d-move" ratio="2" />
     </div>
 </template>
