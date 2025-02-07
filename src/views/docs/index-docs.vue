400a71a9940eb21aa1a7bb1309122603fe8b121d
prfardin <prfardin@gmail.com>
2025-02-07 07:06:54 +0330

    icon huge solid regular fix

--- src/views/docs/index-docs.vue
+++ src/views/docs/index-docs.vue
@@ -7,29 +7,10 @@ import PrIcon from '@c/core/PrIcon.vue'
 
 <template>
     <div class="uk-background-default">
-        <pr-icon icon="icon-huge-bulk-3d-scale" ratio="2" />
-        <pr-icon icon="icon-huge-bulk-abacus" ratio="2" />
-        <pr-icon icon="icon-huge-bulk-church" ratio="2" />
-        <pr-icon icon="icon-huge-bulk-teaching" ratio="2" />
         <pr-icon icon="icon-huge-bulk-google-lens" ratio="2" />
-        <pr-icon icon="icon-huge-duotone-3d-scale" ratio="2" />
-        <pr-icon icon="icon-huge-duotone-abacus" ratio="2" />
-        <pr-icon icon="icon-huge-duotone-church" ratio="2" />
-        <pr-icon icon="icon-huge-duotone-google-lens" ratio="2" />
-        <pr-icon icon="icon-huge-duotone-1st-bracket" ratio="2" />
-        <pr-icon icon="icon-huge-duotone-1st-bracket-circle" ratio="2" />
-        <pr-icon icon="icon-huge-duotone-video-off" ratio="2" />
-        <pr-icon icon="icon-huge-duotone-lighthouse" ratio="2" />
-        <pr-icon icon="icon-huge-duotone-champion" ratio="2" />
-        <pr-icon icon="icon-huge-duotone-drag-right-03" ratio="2" />
-        <pr-icon icon="icon-huge-regular-3d-scale" ratio="2" />
-        <pr-icon icon="icon-huge-regular-3d-rotate" ratio="2" />
-        <pr-icon icon="icon-huge-regular-activity-01" ratio="2" />
-        <pr-icon icon="icon-huge-regular-video-01" ratio="2" />
-        <pr-icon icon="icon-huge-regular-filter" ratio="2" />
-        <pr-icon icon="icon-huge-rounded-filter" ratio="2" />
-        <pr-icon icon="icon-huge-regular-wink" ratio="2" />
-        <pr-icon icon="icon-huge-rounded-wink" ratio="2" />
-        <pr-icon icon="icon-huge-sharp-call-paused" ratio="2" />
+        <pr-icon icon="icon-huge-solidre-google-lens" ratio="2" />
+        <pr-icon icon="icon-huge-solidre-berlin" ratio="2" />
+        <pr-icon icon="icon-huge-solidre-3-d-view" ratio="2" />
+        <pr-icon icon="icon-huge-solidre-bubble-chat-blocked" ratio="2" />
     </div>
 </template>
