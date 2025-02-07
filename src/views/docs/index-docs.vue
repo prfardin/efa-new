3535b1411f451f909da9ed0577f55ede69d4f620
prfardin <prfardin@gmail.com>
2025-02-08 02:36:00 +0330

    duotone icons fix

--- src/views/docs/index-docs.vue
+++ src/views/docs/index-docs.vue
@@ -9,6 +9,8 @@ import PrIcon from '@c/core/PrIcon.vue'
     <div class="uk-background-default">
         <pr-icon icon="icon-huge-bulk-google-lens" ratio="2" />
         <pr-icon icon="icon-solar-broken-4k" ratio="2" />
-        <pr-icon icon="icon-solar-broken-adhesive-plaster-2" ratio="2" />
+        <pr-icon icon="icon-solar-duotone-4k" ratio="2" />
+        <pr-icon icon="icon-solar-duotone-arrows-reorder" ratio="2" />
+        <pr-icon icon="icon-solar-duotone-bedside-table-3" ratio="2" />
     </div>
 </template>
