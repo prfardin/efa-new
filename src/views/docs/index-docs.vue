6d92d0338daa22b5148c11a3a7a2be4bc7314bdf
prfardin <prfardin@gmail.com>
2025-02-07 17:39:09 +0330

    huge solid rounded icons fix

--- src/views/docs/index-docs.vue
+++ src/views/docs/index-docs.vue
@@ -11,6 +11,9 @@ import PrIcon from '@c/core/PrIcon.vue'
         <pr-icon icon="icon-huge-solidre-google-lens" ratio="2" />
         <pr-icon icon="icon-huge-solidre-berlin" ratio="2" />
         <pr-icon icon="icon-huge-solidre-3-d-view" ratio="2" />
+        <pr-icon icon="icon-huge-solidro-3-d-view" ratio="2" />
         <pr-icon icon="icon-huge-solidre-bubble-chat-blocked" ratio="2" />
+        <pr-icon icon="icon-huge-solidro-bubble-chat-blocked" ratio="2" />
+        <pr-icon icon="icon-huge-solidro-1st-bracket-circle" ratio="2" />
     </div>
 </template>
