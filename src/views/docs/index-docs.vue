bb28a4b73aabb127115573b976f78d19231a19cb
prfardin <prfardin@gmail.com>
2025-02-09 00:01:14 +0330

    solar duotone icons fix

--- src/views/docs/index-docs.vue
+++ src/views/docs/index-docs.vue
@@ -7,21 +7,21 @@ import PrIcon from '@c/core/PrIcon.vue'
 
 <template>
     <div class="uk-background-default">
-        <pr-icon icon="icon-huge-bulk-google-lens" ratio="2" />
-        <pr-icon icon="icon-huge-rounded-atom-02" ratio="2" />
-        <pr-icon icon="icon-md-twotone-4k" ratio="2" />
-        <pr-icon icon="icon-md-twotone-15mp" ratio="2" />
-        <pr-icon icon="icon-ming-duotone-currency-dollar" ratio="2" />
-        <pr-icon icon="icon-ming-duotone-text-direction-left" ratio="2" />
-        <pr-icon icon="icon-ming-duotone-abs" ratio="2" />
-        <pr-icon icon="icon-ming-duotone-align-arrow-down" ratio="2" />
-        <pr-icon icon="icon-ming-duotone-building-5" ratio="2" />
-        <pr-icon icon="icon-ming-duotone-egyptian-pyramids" ratio="2" />
-        <pr-icon icon="icon-ming-duotone-text-2" ratio="2" />
-        <pr-icon icon="icon-ming-twotone-monitor" ratio="2" />
-        <pr-icon icon="icon-ming-twotone-abs" ratio="2" />
-        <pr-icon icon="icon-ming-twotone-ZA-sort-ascending-letters" ratio="2" />
-        <pr-icon icon="icon-ming-twotone-hood" ratio="2" />
-        <pr-icon icon="icon-ph-duotone-acorn" ratio="2" />
+        <pr-icon icon="icon-solar-duotone-ufo-2" ratio="2" />
+        <pr-icon icon="icon-solar-duotone-magnifer-bug" ratio="2" />
+        <pr-icon icon="icon-solar-duotone-magnifer-zoom-in" ratio="2" />
+        <pr-icon icon="icon-solar-duotone-magnifer-zoom-out" ratio="2" />
+        <pr-icon icon="icon-solar-duotone-magnifer" ratio="2" />
+        <pr-icon icon="icon-solar-duotone-minimalistic-magnifer-zoom-in" ratio="2" />
+        <pr-icon icon="icon-solar-duotone-minimalistic-magnifer-bug" ratio="2" />
+        <pr-icon icon="icon-solar-duotone-minimalistic-magnifer-zoom-out" ratio="2" />
+        <pr-icon icon="icon-solar-duotone-minimalistic-magnifer" ratio="2" />
+        <pr-icon icon="icon-solar-duotone-rounded-magnifer-bug" ratio="2" />
+        <pr-icon icon="icon-solar-duotone-rounded-magnifer-zoom-in" ratio="2" />
+        <pr-icon icon="icon-solar-duotone-satellite" ratio="2" />
+        <pr-icon icon="icon-solar-duotone-ufo-2" ratio="2" />
+        <pr-icon icon="icon-solar-duotone-ufo-3" ratio="2" />
+        <pr-icon icon="icon-solar-duotone-ufo" ratio="2" />
+        <pr-icon icon="icon-solar-duotone-zip-file" ratio="2" />
     </div>
 </template>
