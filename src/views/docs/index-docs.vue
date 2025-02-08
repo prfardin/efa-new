af89d17037d32b340938aaea2ae8a784f9dd6552
prfardin <prfardin@gmail.com>
2025-02-09 03:19:32 +0330

    solar duotone solid icons fix

--- src/views/docs/index-docs.vue
+++ src/views/docs/index-docs.vue
@@ -9,5 +9,9 @@ import PrIcon from '@c/core/PrIcon.vue'
     <div class="uk-background-default">
         <pr-icon icon="icon-solar-duotone-accessibility" ratio="2" />
         <pr-icon icon="icon-solar-duotoneso-accessibility" ratio="2" />
+        <pr-icon icon="icon-solar-duotoneso-black-hole" ratio="2" />
+        <pr-icon icon="icon-solar-duotone-black-hole" ratio="2" />
+        <pr-icon icon="icon-solar-duotoneso-sort-from-top-to-bottom" ratio="2" />
+        <pr-icon icon="icon-solar-duotoneso-camera-minimalistic" ratio="2" />
     </div>
 </template>
