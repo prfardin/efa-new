17f7feba349a046cf7c4f5090d8e4e502b3d40b9
prfardin <prfardin@gmail.com>
2025-02-09 02:37:04 +0330

    solar duotone icons fix

--- src/views/docs/index-docs.vue
+++ src/views/docs/index-docs.vue
@@ -7,21 +7,7 @@ import PrIcon from '@c/core/PrIcon.vue'
 
 <template>
     <div class="uk-background-default">
-        <pr-icon icon="icon-solar-duotone-ufo-2" ratio="2" />
-        <pr-icon icon="icon-solar-duotone-magnifer-bug" ratio="2" />
-        <pr-icon icon="icon-solar-duotone-magnifer-zoom-in" ratio="2" />
-        <pr-icon icon="icon-solar-duotone-magnifer-zoom-out" ratio="2" />
-        <pr-icon icon="icon-solar-duotone-magnifer" ratio="2" />
-        <pr-icon icon="icon-solar-duotone-minimalistic-magnifer-zoom-in" ratio="2" />
-        <pr-icon icon="icon-solar-duotone-minimalistic-magnifer-bug" ratio="2" />
-        <pr-icon icon="icon-solar-duotone-minimalistic-magnifer-zoom-out" ratio="2" />
-        <pr-icon icon="icon-solar-duotone-minimalistic-magnifer" ratio="2" />
-        <pr-icon icon="icon-solar-duotone-rounded-magnifer-bug" ratio="2" />
-        <pr-icon icon="icon-solar-duotone-rounded-magnifer-zoom-in" ratio="2" />
-        <pr-icon icon="icon-solar-duotone-satellite" ratio="2" />
-        <pr-icon icon="icon-solar-duotone-ufo-2" ratio="2" />
-        <pr-icon icon="icon-solar-duotone-ufo-3" ratio="2" />
-        <pr-icon icon="icon-solar-duotone-ufo" ratio="2" />
-        <pr-icon icon="icon-solar-duotone-zip-file" ratio="2" />
+        <pr-icon icon="icon-solar-duotone-accessibility" ratio="2" />
+        <pr-icon icon="icon-solar-duotoneso-accessibility" ratio="2" />
     </div>
 </template>
