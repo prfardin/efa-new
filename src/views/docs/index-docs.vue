b4a6d73d7c6304323b6a6637837fdd6a3797b2a9
prfardin <prfardin@gmail.com>
2025-02-10 03:10:39 +0330

    latest changes

--- src/views/docs/index-docs.vue
+++ src/views/docs/index-docs.vue
@@ -7,7 +7,22 @@ import PrIcon from '@c/core/PrIcon.vue'
 
 <template>
     <div class="uk-background-default">
-        <pr-icon icon="icon-uni-mono-airplay" ratio="2" />
-        <pr-icon icon="icon-magi-duotone-airplay" ratio="2" />
+        <pr-icon icon="icon-fa-duotone-filters" ratio="2" />
+        <pr-icon icon="icon-fa-duotone-fingerprint" ratio="2" />
+        <pr-icon icon="icon-fa-duotoneli-fingerprint" ratio="2" />
+        <pr-icon icon="icon-fa-duotoneso-fingerprint" ratio="2" />
+        <pr-icon icon="icon-fa-duotoneth-fingerprint" ratio="2" />
+        <pr-icon icon="icon-bulk-duotone-finger-print" ratio="2" />
+        <pr-icon icon="icon-huge-duotone-finger-print" ratio="2" />
+        <pr-icon icon="icon-huge-twotone-finger-print" ratio="2" />
+        <pr-icon icon="icon-magi-duotone-fingerprint" ratio="2" />
+        <pr-icon icon="icon-md-twotone-fingerprint" ratio="2" />
+        <pr-icon icon="icon-ming-duotone-fingerprint" ratio="2" />
+        <pr-icon icon="icon-ming-twotone-fingerprint" ratio="2" />
+        <pr-icon icon="icon-ming-duotone-fingerprint-2" ratio="2" />
+        <pr-icon icon="icon-ming-twotone-fingerprint-2" ratio="2" />
+        <pr-icon icon="icon-ph-duotone-fingerprint" ratio="2" />
+        <pr-icon icon="icon-solar-duotone-user-hands" ratio="2" />
+        <pr-icon icon="icon-solar-duotoneso-user-hands" ratio="2" />
     </div>
 </template>
