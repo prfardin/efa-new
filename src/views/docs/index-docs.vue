765d9ab42fdce508398caa4f2a0fc38f26fb0199
prfardin <prfardin@gmail.com>
2025-02-16 00:45:55 +0330

    isocons icons fix

--- src/views/docs/index-docs.vue
+++ src/views/docs/index-docs.vue
@@ -7,23 +7,11 @@ import PrIcon from '@c/core/PrIcon.vue'
 
 <template>
     <div class="uk-background-default">
-        <pr-icon icon="icon-isocons-duotoneleft-6-ft-apart-515" ratio="2" />
-        <pr-icon icon="icon-isocons-duotonetop-6-ft-apart-515" ratio="2" />
-        <pr-icon icon="icon-isocons-duotoneright-6-ft-apart-515" ratio="2" />
-        <pr-icon icon="icon-isocons-duotonesharpleft-6-ft-apart-515" ratio="2" />
-        <pr-icon icon="icon-isocons-duotonesharptop-6-ft-apart-515" ratio="2" />
-        <pr-icon icon="icon-isocons-duotonesharpright-6-ft-apart-515" ratio="2" />
-        <pr-icon icon="icon-isocons-left-6-ft-apart-515" ratio="2" />
-        <pr-icon icon="icon-isocons-top-6-ft-apart-515" ratio="2" />
-        <pr-icon icon="icon-isocons-right-6-ft-apart-515" ratio="2" />
-        <pr-icon icon="icon-isocons-sharpleft-6-ft-apart-515" ratio="2" />
-        <pr-icon icon="icon-isocons-sharptop-6-ft-apart-515" ratio="2" />
-        <pr-icon icon="icon-isocons-sharpright-6-ft-apart-515" ratio="2" />
-        <pr-icon icon="icon-isocons-solidleft-6-ft-apart-515" ratio="2" />
-        <pr-icon icon="icon-isocons-solidtop-6-ft-apart-515" ratio="2" />
-        <pr-icon icon="icon-isocons-solidright-6-ft-apart-515" ratio="2" />
-        <pr-icon icon="icon-isocons-solidsharpleft-6-ft-apart-515" ratio="2" />
-        <pr-icon icon="icon-isocons-solidsharptop-6-ft-apart-515" ratio="2" />
-        <pr-icon icon="icon-isocons-solidsharpright-6-ft-apart-515" ratio="2" />
+        <pr-icon icon="icon-isocons-duotonetop-123" ratio="2" />
+        <pr-icon icon="icon-isocons-duotonesharptop-123" ratio="2" />
+        <pr-icon icon="icon-isocons-top-123" ratio="2" />
+        <pr-icon icon="icon-isocons-sharptop-123" ratio="2" />
+        <pr-icon icon="icon-isocons-solidtop-123" ratio="2" />
+        <pr-icon icon="icon-isocons-solidsharptop-123" ratio="2" />
     </div>
 </template>
